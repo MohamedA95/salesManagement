227227a3e0de560a5cabafbff0cf3c7075cc9635
habi3000 <mahmedgamal995@gmail.com>
2018-12-12 15:59:46 +0300

    update

--- salesManagement/main/models.py
+++ salesManagement/main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class commission(models.Model):
+    typestr=models.CharField(max_length=100,default='',null=False, blank=False)
+    saleprice=models.FloatField()
+class product(models.Model):
+    name=models.CharField(max_length=100,default='',null=False,unique=True, blank=False)
+    typestr=models.ForeignKey(commission,on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='ProductsImages',blank=True)
+    description=models.CharField(max_length=1000,default='')
+
+class batch(models.Model):
+    productname=models.ForeignKey(product,on_delete=models.SET_DEFAULT,default='')
+    quant=models.IntegerField()
+    unitprice=models.FloatField()
+    uid=models.CharField(max_length=100,default='',null=False,blank=False)
+class sales(models.Model):
+    productname=models.ForeignKey(product,on_delete=models.SET_DEFAULT,default='')
+    quant=models.IntegerField()
+    saleprice=models.FloatField()
+    batchid=models.ForeignKey(batch,on_delete=models.SET_DEFAULT,default='')
+    profit=models.FloatField()
+        
--- salesManagement/main/views.py
+++ salesManagement/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def addpro(request):
+    return render(request,'addproduct.html')
+def rmit(request):
+    return render(request,'removeitem.html')
+def addit(request):
+    return render(request,'additem.html')
+def rep(request):
+    return render(request,'reports.html')
+def home(request):
+    return render(request,'home.html')
